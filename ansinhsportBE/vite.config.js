375d55885d11008f85221af7d0036d34585bfd73
sonpham <dinhson.ansinh@gmail.com>
2024-09-09 15:46:16 +0700

    commit

--- ansinhsportBE/vite.config.js
+++ ansinhsportBE/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/<ansinhsportBE>/",
+  base: "ansinhsportBE",
   define: {
     "process.env": {
       BASE_PATH: process.env.VITE_BASE_PATH,
